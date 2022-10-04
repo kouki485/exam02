cfd70060618c7118c014cb471ca459c93082fe01
Kaida Kohki <kkohki@c1r17s9.42tokyo.jp>
2022-10-05 07:32:38 +0900

    fin2

--- level2/snake_to_camel.c
+++ level2/snake_to_camel.c
@@ -1,18 +1,17 @@
 #include <libc.h>
 
-int main(int argc, char **argv)
+int main(int argc,char **argv)
 {
-	if (argc == 2)
+	if(argc == 2)
 	{
-		for(int i = 0;argv[1][i];i++)
+		for (int i = 0;argv[1][i];i++)
 		{
-			if(argv[1][i] == '_' )
+			if (argv[1][i] == '_')
 			{
 				i++;
-				if(97 <= argv[1][i] && argv[1][i] <= 122)
-					argv[1][i] -= 32;
+				argv[1][i] -= 32;
 			}
-				write(1,&argv[1][i],1);
+			write(1,&argv[1][i],1);
 		}
 	}
 	write(1,"\n",1);
--- level2/wdmatch.c
+++ level2/wdmatch.c
@@ -1,3 +1,23 @@
 #include <libc.h>
 
-int main()
+void ft_putstr(char const *str)
+{
+	int i = 0;
+
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int main(int argc,char **argv)
+{
+	int n = 0;
+	if (argc == 3)
+	{
+		for(int i = 0;argv[2][i];i++)
+				if(argv[2][i] == argv[1][n])
+					n++;
+		if (!argv[1][n])
+			ft_putstr(argv[1]);
+	}
+	write(1,"\n",1);
+}
