00a57359b1e01a214a060fddaac76f3b55aaafd7
kouki485 <mwd3145@gmail.com>
2022-10-07 12:09:53 +0900

    hello

--- level3/expand_str.c
+++ level3/expand_str.c
@@ -2,25 +2,25 @@
 
 int tos(char c)
 {
-	return (c == ' ' | c == '\t');
+	return(c == ' ' || c == '\t');
 }
 
 int main(int argc,char **argv)
 {
 	int i = 0;
-	int flag = 0;
-	if(argc == 2)
+	int flag;
+	if (argc == 2)
 	{
 		while(tos(argv[1][i]))
-				i++;
+			i++;
 		while(argv[1][i])
 		{
-			if(tos(argv[1][i]))
+			if (tos(argv[1][i]))
 				flag = 1;
-			if(!tos(argv[1][i]))
+			if (!tos(argv[1][i]))
 			{
 				if(flag)
-					write(1," ",1);
+					write(1,"   ",3);
 				flag = 0;
 				write(1,&argv[1][i],1);
 			}
